b2e6eeb8c836810474c2ca74b4a623c0cc24f0c5
Potaychuk Sviatoslav <sviatoslavpotaychuk@gmail.com>
2017-03-12 22:32:17 +0200

    added scripts for db init

---
+++ src/main/resources/dbInit/createFewUsers.sql
@@ -0,0 +1,5 @@
+insert into user  (login,password,role,email) values ('admin','admin','admin','admin@gmail.com');
+insert into user  (login,password,role,email) values ('Anna','admin','customer','demoanna@gmail.com');
+insert into user  (login,password,role,email) values ('Peter','peterthebest','moderator','peter86@mail.ru');
+insert into user  (login,password,role,email) values ('Finn','demopass','factory','finnthehuman@gmail.com');
+insert into user  (login,password,role,email) values ('Karla','pass','moderator','turk@gmail.com');
